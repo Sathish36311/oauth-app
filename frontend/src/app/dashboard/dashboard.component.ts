162c890d366763c78f79af8e594386e7ce4bcc87
Sathish Krishnan <sathishkrishnan369@gmail.com>
2025-08-07 20:45:32 +0530

    Added Logo, NavigationBar and Updated dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { NavComponent } from '../shared/nav/nav.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
